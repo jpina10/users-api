33e54cbe8267cc0322157dc50e34577e0ec9ccd8
jpina <joao_pinaa@hotmail.com>
2023-11-08 13:47:57 +0000

    add configuration and repository

---
+++ src/main/java/com/users/api/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.users.api.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr353.JSR353Module;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("Basic authentication", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info()
+                        .title("Users API")
+                        .description("CRUD API for users")
+                        .summary("summary").version("v1"));
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JSR353Module());
+        objectMapper.registerModule(new JSR310Module());
+
+        return objectMapper;
+    }
+}
