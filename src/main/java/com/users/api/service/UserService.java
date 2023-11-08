98eb0d9af64b0ea076beb2709c8c05e302ed95a5
jpina <joao_pinaa@hotmail.com>
2023-11-08 13:46:08 +0000

    add userService

---
+++ src/main/java/com/users/api/service/UserService.java
@@ -0,0 +1,15 @@
+package com.users.api.service;
+
+import com.users.api.dto.UserDto;
+
+import javax.json.JsonPatch;
+import java.util.List;
+
+public interface UserService {
+    UserDto findUserByUserName(String username);
+    String createRandomUser();
+    void enableUser(String username);
+    void deleteUser(String username);
+    List<UserDto> getAllUsers();
+    void updateUser(String username, JsonPatch jsonPatch);
+}
