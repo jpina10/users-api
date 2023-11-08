b950f47bb2539af16ab2e255f038aa57c2ea81ee
jpina <joao_pinaa@hotmail.com>
2023-11-08 13:45:37 +0000

    add random name api client

---
+++ src/main/java/com/users/api/nameapi/model/Error.java
@@ -0,0 +1,18 @@
+package com.users.api.nameapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Error {
+
+    @JsonProperty("error")
+    private String errorMessage;
+}
