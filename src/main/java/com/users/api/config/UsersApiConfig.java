4d80adef9cf8195b14458890905c880a46ccb6e9
pina <joao_pinaa@hotmail.com>
2024-01-02 18:25:39 +0000

    fix unit tests rename config class

--- src/main/java/com/users/api/config/UsersApiConfig.java
+++ src/main/java/com/users/api/config/UsersApiConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class UsersApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
